52837135762b6925440ee54dbebdb627f79621e1
holydoh <holydoh@users.noreply.github.com>
2024-04-28 15:14:36 +0200

    Message de validation des modifications

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+// Chemins des liens des projets
+var projet1 = "projet_dvf.html";
+var projet2 = "projet_aqua.html";
+var projet3 = "projet_salle.html";
+
 document.addEventListener("DOMContentLoaded", function() {
     // Chargement du Header
     var xhrHeader = new XMLHttpRequest();
     xhrHeader.onreadystatechange = function() {
         if (xhrHeader.readyState === XMLHttpRequest.DONE) {
             if (xhrHeader.status === 200) {
-                document.getElementById("header").innerHTML = xhrHeader.responseText;
+                // Remplacez les marqueurs spéciaux par les valeurs des variables
+                var headerContent = xhrHeader.responseText;
+                headerContent = headerContent.replace('{{projet1}}', projet1);
+                headerContent = headerContent.replace('{{projet2}}', projet2);
+                headerContent = headerContent.replace('{{projet3}}', projet3);
+                
+                document.getElementById("header").innerHTML = headerContent;
             } else {
                 console.error("Erreur de chargement du header : " + xhrHeader.status);
             }
@@ -12,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
     };
     xhrHeader.open("GET", "header.html", true);
     xhrHeader.send();
-
+    
     // Chargement du Footer
     var xhrFooter = new XMLHttpRequest();
     xhrFooter.onreadystatechange = function() {
@@ -26,9 +37,7 @@ document.addEventListener("DOMContentLoaded", function() {
     };
     xhrFooter.open("GET", "footer.html", true);
     xhrFooter.send();
-});
 
-document.addEventListener("DOMContentLoaded", function() {
     var items = document.querySelectorAll(".projet_chapitre_section");
 
     items.forEach(function(item) {
@@ -37,3 +46,11 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+// Modification des liens dans le fichier HTML
+document.getElementById("lien_projet1").href = projet1;
+document.getElementById("lien_projet2").href = projet2;
+document.getElementById("lien_projet3").href = projet3;
+document.getElementById("lien_img_projet1").href = projet1;
+document.getElementById("lien_img_projet2").href = projet2;
+document.getElementById("lien_img_projet3").href = projet3;
