3bcac8f4dd9586770a2f4bf4c474707bbb5481fd
holydoh <holydoh@users.noreply.github.com>
2024-04-28 11:33:56 +0200

    Message de validation des modifications

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Chargement du Header
+    var xhrHeader = new XMLHttpRequest();
+    xhrHeader.onreadystatechange = function() {
+        if (xhrHeader.readyState === XMLHttpRequest.DONE) {
+            if (xhrHeader.status === 200) {
+                document.getElementById("header").innerHTML = xhrHeader.responseText;
+            } else {
+                console.error("Erreur de chargement du header : " + xhrHeader.status);
+            }
+        }
+    };
+    xhrHeader.open("GET", "header.html", true);
+    xhrHeader.send();
+
+    // Chargement du Footer
+    var xhrFooter = new XMLHttpRequest();
+    xhrFooter.onreadystatechange = function() {
+        if (xhrFooter.readyState === XMLHttpRequest.DONE) {
+            if (xhrFooter.status === 200) {
+                document.getElementById("footer").innerHTML = xhrFooter.responseText;
+            } else {
+                console.error("Erreur de chargement du footer : " + xhrFooter.status);
+            }
+        }
+    };
+    xhrFooter.open("GET", "footer.html", true);
+    xhrFooter.send();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
     var items = document.querySelectorAll(".projet_chapitre_section");
 
     items.forEach(function(item) {
